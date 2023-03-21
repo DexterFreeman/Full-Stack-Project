4045cdfbf4ce0415e7b79361b9633b3aeefc27c6
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-03-21 16:45:33 +0000

    Switched data from js to json

--- bbx-frontend/src/pages/Home/Home.jsx
+++ bbx-frontend/src/pages/Home/Home.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import './Home.scss';
-import beatboxers from "../../data/data"
+import data from '../../data/data.json';
 import BeatboxerCard from '../../components/BeatboxerCard/BeatboxerCard';
 const Home = () => {
-  console.log(beatboxers)
-  const test = beatboxers[0]
-  const test2 = beatboxers[1]
+  console.log(data)
   return (
     <div className="home-page">
         <header className='home-page__header'>
           <h1>Home page</h1>
         </header>
         <div className="home-page__cards">
-          <BeatboxerCard name={test.name} nationality={test.nationality} realName={test.real_name} achievements={test.achievements} sounds={test.notable_sounds} />
-          <BeatboxerCard name={test2.name} nationality={test2.nationality} realName={test2.real_name} achievements={test2.achievements} sounds={test2.notable_sounds} />
+          <BeatboxerCard name={data[0].name} nationality={data[0].nationality} realName={data[0].real_name} achievements={data[0].achievements} sounds={data[0].notable_sounds} />
+          <BeatboxerCard name={data[1].name} nationality={data[1].nationality} realName={data[1].real_name} achievements={data[1].achievements} sounds={data[1].notable_sounds} /> 
         </div>
        
     </div>
